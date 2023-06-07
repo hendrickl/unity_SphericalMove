9b1e6ed5ce023c747b43f61494c1e4567cd154fa
hendrickl <hobby@hendricks-MacBook-Air.local>
2023-06-07 15:52:03 +0200

    Switch player rotation direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed = 15f;
 
+    [SerializeField]
+    private float _moveSpeed = 1f;
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+            transform.Rotate(-Vector3.up, _rotationSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.up, _rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
         }
     }
 }
