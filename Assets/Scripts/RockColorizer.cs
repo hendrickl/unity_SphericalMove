d81f250dbbcb741b8149ebd3ea3d865763e930b4
hendrickl <hobby@hendricks-MacBook-Air.local>
2023-06-08 07:40:15 +0200

    HUD : Display number of white and black cubes

--- Assets/Scripts/RockColorizer.cs
+++ Assets/Scripts/RockColorizer.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class RockColorizer : MonoBehaviour
 {
+    public static RockColorizer instance;
+    public int numberOfWhiteRocks;
+    public int numberOfBlackRocks;
+
+    private void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            Debug.Log("RockColorizer instance initialized " + instance);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("ColoredCube"))
@@ -9,6 +26,8 @@ public class RockColorizer : MonoBehaviour
             Debug.Log("OntriggerENTER detected");
             other.gameObject.GetComponent<Renderer>().material.color = Color.black;
         }
+        ComputeNumberOfWhiteRocks();
+        ComputeNumberOfBlackRocks();
     }
 
     private void OnTriggerExit(Collider other)
@@ -18,5 +37,32 @@ public class RockColorizer : MonoBehaviour
             Debug.Log("OntriggerEXIT detected");
             other.gameObject.GetComponent<Renderer>().material.color = Color.white;
         }
+        ComputeNumberOfWhiteRocks();
+        ComputeNumberOfBlackRocks();
+    }
+
+    public int ComputeNumberOfWhiteRocks()
+    {
+        numberOfWhiteRocks = 0;
+
+        GameObject[] rocks = GameObject.FindGameObjectsWithTag("ColoredCube");
+
+        foreach (GameObject rock in rocks)
+        {
+            if (rock.GetComponent<Renderer>().material.color == Color.white)
+            {
+                numberOfWhiteRocks++;
+                Debug.Log("Number of white rocks :" + numberOfWhiteRocks);
+            }
+        }
+        return numberOfWhiteRocks;
+    }
+
+    public int ComputeNumberOfBlackRocks()
+    {
+        numberOfBlackRocks = 0;
+        numberOfBlackRocks = RockSpawner.instance.nbOfRocks - numberOfWhiteRocks;
+        Debug.Log("Number of black rocks :" + numberOfBlackRocks);
+        return numberOfBlackRocks;
     }
 }
--- Assets/Scripts/RockStatistics.cs
+++ Assets/Scripts/RockStatistics.cs
@@ -3,21 +3,33 @@ using TMPro;
 
 public class RockStatistics : MonoBehaviour
 {
-	[SerializeField]
-	private TMP_Text m_leftResult;
+    [SerializeField]
+    private TMP_Text m_leftResult;
 
-	[SerializeField]
-	private TMP_Text m_rightResult;
+    [SerializeField]
+    private TMP_Text m_rightResult;
 
-	public struct Statistics
-	{
-		public int left;
-		public int right;
-	}
+    public struct Statistics
+    {
+        public int left;
+        public int right;
+    }
 
-	public void UpdateStatistics(Statistics p_stats)
-	{
-		m_leftResult.text = p_stats.left.ToString();
-		m_rightResult.text = p_stats.right.ToString();
-	}
+    private void Update()
+    {
+        Debug.Log("Nb of rocks from RockStatistics " + RockSpawner.instance.nbOfRocks);
+        DisplayStatistics();
+    }
+
+    public void UpdateStatistics(Statistics p_stats)
+    {
+        m_leftResult.text = p_stats.left.ToString();
+        m_rightResult.text = p_stats.right.ToString();
+    }
+
+    private void DisplayStatistics()
+    {
+        m_leftResult.text = RockColorizer.instance.numberOfBlackRocks.ToString();
+        m_rightResult.text = RockColorizer.instance.numberOfWhiteRocks.ToString();
+    }
 }
